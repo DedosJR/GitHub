2ed4c271dcb56e68b4de73cb9478733a3f4cf4ce
Israel <dedos_jr@hotmail.com>
2025-04-10 13:28:03 -0700

    Add changes 10-04-25

---
+++ Python/Curso/Demo.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 16:48:09 2019
+@author: Israel Flores 
+"""
+from sre_parse import IN
+
+
+msg = "Hola mundo"
+print(msg)
+
+saludo = (input("Hola, Dame tu nombre: "))
+print("Hola " + saludo + " como estas?")
